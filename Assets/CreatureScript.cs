d708fe9450d9ed563de6e28bafd96f74f6327851
John Mendiola <johnnywendy@gmail.com>
2014-05-03 15:03:21 -0500

    Added prefabs

--- Assets/CreatureScript.cs
+++ Assets/CreatureScript.cs
@@ -3,9 +3,14 @@ using System.Collections;
 
 public class CreatureScript : MonoBehaviour {
 
+	public int typeInt;
+	//if 0, defaults to random type
+
 	// Use this for initialization
 	void Start () {
-	
+		if (typeInt == 0) {
+			print ("hello");		
+		}
 	}
 	
 	// Update is called once per frame
