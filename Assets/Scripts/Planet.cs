02abf53a3c5843c38b1673708689eb0faaf183b8
hmcdona1 <gmail@reecemcdonald.com>
2014-05-03 17:27:56 -0500

    idk

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -5,6 +5,7 @@ public class Planet : MonoBehaviour {
 	
 	public bool inBounds = false;
 	private bool start = true;
+	private ArrayList radiusPoints;
 
 	// Use this for initialization
 	void Start () {
@@ -18,12 +19,25 @@ public class Planet : MonoBehaviour {
 		transform.localScale = new Vector3(transform.localScale.x+modifier,transform.localScale.y+modifier,transform.localScale.z);
 		transform.rotation = Quaternion.Euler(0f,0f,Random.Range(0,360));
 		int planetType = Random.Range(0,6);
+
+		float radius = GetComponents<CircleCollider2D>()[0].radius*transform.localScale.x+(0.125f*transform.localScale.x);
+		float numPoints = Mathf.Floor((radius*4)/2)*2;
+		float step = (Mathf.PI*2) / numPoints;
+		float current = 0;
+		radiusPoints = new ArrayList();
+		for (int i = 0; i < numPoints; i++) {
+			Debug.Log(new Vector2(Mathf.Sin(current) * radius,Mathf.Cos(current) * radius));
+			Debug.Log(radius);
+			radiusPoints.Add(new Vector2(Mathf.Sin(current) * radius,Mathf.Cos(current) * radius));
+			current += step;
+		}
+
 		if (planetType == 0)
 			SetupMoon1();
 		if (planetType == 1)
 			SetupMoon2();
 		if (planetType == 2)
-			SetupGrass1();
+			SetupSlime1();
 		if (planetType == 3)
 			SetupGrass2();
 		if (planetType == 4)
@@ -53,6 +67,8 @@ public class Planet : MonoBehaviour {
 	void SetupMoon1() {
 		renderer.material = Resources.Load("moon_border_1", typeof(Material)) as Material;
 		transform.FindChild("Texture").gameObject.renderer.material.mainTexture = Resources.Load("moon_1", typeof(Texture)) as Texture;
+		Vector2 pos = (Vector2)radiusPoints[0];
+		Instantiate(Resources.Load("abc", typeof(GameObject)),new Vector3(transform.position.x+pos.x,transform.position.y+pos.y,2f),Quaternion.identity);
 	}
 
 	void SetupMoon2() {
@@ -79,5 +95,11 @@ public class Planet : MonoBehaviour {
 		renderer.material = Resources.Load("ice_border_1", typeof(Material)) as Material;
 		transform.FindChild("Texture").gameObject.renderer.material.mainTexture = Resources.Load("ice_1", typeof(Texture)) as Texture;
 		GetComponents<CircleCollider2D>()[0].sharedMaterial = Resources.Load("IcySurface", typeof(PhysicsMaterial2D)) as PhysicsMaterial2D;
+	}
+
+	void SetupSlime1() {
+		renderer.material = Resources.Load("slime_border_1", typeof(Material)) as Material;
+		transform.FindChild("Texture").gameObject.renderer.material.mainTexture = Resources.Load("slime_1", typeof(Texture)) as Texture;
+		GetComponents<CircleCollider2D>()[0].sharedMaterial = Resources.Load("BouncySurface", typeof(PhysicsMaterial2D)) as PhysicsMaterial2D;
 	}
 }
